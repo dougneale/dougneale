a890dd9b4c13b26015bc6caf43940d538e6900f4
Doug Neale <doug.neale@originenergy.com.au>
2017-11-04 22:41:45 +1100

    Remove unneeded css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Personal website coming soon</h1>
-        </header>
+      <div>
+          <h1>Personal website coming soon</h1>
       </div>
     );
   }
