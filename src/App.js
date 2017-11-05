ee3713be3ee8370dd44120c141fd653758752bfe
Doug <dougneale@gmail.com>
2017-11-05 14:40:13 +1100

    fix bugs

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 
 const App = ({className}) => (
   <div className={className}>
-    <h1> I'm Doug Neale 🤝</h1>
+    <h1> I'm Doug Neale <span role="img" aria-label="handshake">🤝</span></h1>
     <hr />
     <h2> I'm a technologist <span role="img" aria-label="male technologist">👨‍💻</span> at <a href="https://www.thoughtworks.com/about-us">ThoughtWorks</a></h2>    
     <h2> I lived in Brisbane<span role="img" aria-label="sun">🌞</span> I moved to Melbourne <span role="img" aria-label="tram">🚋</span></h2>
     <h2> Then I started doing improv comedy at <a href="http://improvconspiracy.com">The Improv Conspiracy <span role="img" aria-label="drama">🎭</span></a></h2>
     <h2> I create <a href="https://www.youtube.com/channel/UCVzstBi1Nf5DTlBoc2ioRYg/videos">sketch videos <span role="img" aria-label="camcorder">📹</span></a> and produce <a href="https://soundcloud.com/doug-neale">music <span role="img" aria-label="musical keyboard">🎹</span></a> </h2>
     <h2> I also like to tinker on <a href="https://github.com/dneale">projects</a> like <a href="http://convergence.dougneale.com">Convergence With A Computer <span role="img" aria-label="robot face">🤖</span></a></h2>
-    <h2> I've written <a href="https://www.themanufacturer.com/author/doug-neale/">some articles <span role="img">📰</span></a> at The Manufacturer </h2>
+    <h2> I've written <a href="https://www.themanufacturer.com/author/doug-neale/">some articles <span role="img" aria-label="newspaper">📰</span></a> at The Manufacturer </h2>
     <h3> Find me on 
       <a href="https://twitter.com/dougneale"> Twitter</a>,     
       <a href="https://www.linkedin.com/in/dougneale"> LinkedIn</a>, 
