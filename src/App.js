4182f1eab7cdafc56456cd9230c4ef934a6ef95a
Doug <dougneale@gmail.com>
2017-11-05 12:20:02 +1100

    Basic design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-          <h1>Personal website coming soon</h1>
-      </div>
-    );
-  }
-}
+const App = ({className}) => (
+  <div className={className}>
+    <h1>Hi, I'm Doug</h1>
+    <h2> I lived in Brisbane </h2>
+    <h2> I moved to Melbourne </h2>
+    <h2> I'm a software developer at Thoughtworks </h2>
+    <h2> I do improv comedy at The Improv Conspiracy </h2>
+    <h2> I also like to make projects like this and this </h2>
+    <h2> I also like to make music </h2>
+    <h2> I sometimes write articles </h2>
+    <h2> I've written some articles at The Manufacturer </h2>
+    <h3> I'm on LinkedIn, Twitter, Facebook, and Instagram </h3>
+  </div>
+)
 
-export default App;
+export default styled(App)`
+  margin: auto;
+  padding: 5em;
+  max-width: 700px;
+` ;
